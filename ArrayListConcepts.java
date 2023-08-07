7ae31780d4ba50b96d203fee5f4fa9270b1a1f8e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-07 10:35:36 +0530

    Update ArrayListConcepts.java

--- ArrayListConcepts.java
+++ ArrayListConcepts.java
@@ -37,7 +37,6 @@ public class ArrayListConcepts {
 			String[] name = products.get(i).getText().split("-");
 			String formattedName = name[0].trim();
 			// format it to get actual vegetable name
-			
 			// convert array into array list for easy search
 			
 			// check whether name you extracted is present in arrayList or not-
