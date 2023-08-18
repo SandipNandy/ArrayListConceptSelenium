d1a190bf950f0fcadc3f1f1ef428093558374edd
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-18 09:22:53 +0530

    Update ArrayListConcepts.java

--- ArrayListConcepts.java
+++ ArrayListConcepts.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ArrayListConcepts {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "..\\chromedriver_win32\\chromedriver.exe");
