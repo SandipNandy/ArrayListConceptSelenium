9a0fa46e6dcd2e1bca60077cd4bf3a9448008d0f
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-09-18 09:54:09 +0530

    Update ArrayListConcepts.java

--- ArrayListConcepts.java
+++ ArrayListConcepts.java
@@ -52,6 +52,7 @@ public class ArrayListConcepts {
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		
 		//System.out.println(products.get(0).getText());
+		
 		for (int i = 0; i < products.size(); i++)
 		{
 			// Brocolli - 1 Kg
