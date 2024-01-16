881bad9edef1a3dd84ad49bedf46cd8fcacdd741
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-01-16 09:35:41 +0530

    Update ArrayListConcepts.java

--- ArrayListConcepts.java
+++ ArrayListConcepts.java
@@ -63,6 +63,7 @@ public class ArrayListConcepts {
 			// format it to get actual vegetable name
 			// convert array into array list for easy search
 			// check whether name you extracted is present in arrayList or not-
+			
 			List itemsNeededList = Arrays.asList(itemsNeeded);
 			
 			if (itemsNeededList.contains(formattedName))
