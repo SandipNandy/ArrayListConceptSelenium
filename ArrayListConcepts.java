226db175b450ff653da0b6210a68143079966dca
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-02-19 21:17:55 +0530

    Update ArrayListConcepts.java

--- ArrayListConcepts.java
+++ ArrayListConcepts.java
@@ -25,6 +25,7 @@ public class ArrayListConcepts {
 		//driver.findElement(By.xpath("//button[contains(text(),'ADD TO CART')]")).click();
 		
 		String[] ItemsNeeded= {"Cucumber","Beans","Mushroom","Corn"};
+		
 		for(int i=0;i<ItemsNeeded.length;i++) {
 		driver.findElement(By.xpath("//h4[contains(text(),'"+ItemsNeeded[i]+"')]//following::button[contains(text(),'ADD TO CART')][1]")).click();	
 		}
